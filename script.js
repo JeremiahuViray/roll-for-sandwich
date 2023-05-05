d4e706a877ce38cac9719ba25bedfb30dc2d6050
Jeremiah Viray <jeremiahvirayofficial@gmail.com>
2023-05-04 22:22:06 -0700

    Create UI and Javascript

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const beginBtn = document.querySelector(".begin-btn");
 const modal = document.querySelector(".modal");
 const welcomeModal = document.querySelector(".welcome");
 const rollModal = document.querySelector(".roll-modal");
+const breadModal = document.querySelector(".bread-modal");
+const mainModal = document.querySelector(".main-modal");
 const breadDice = document.querySelector(".bread-roll");
+const mainDice = document.querySelector(".main-roll");
+const breadNextBtn = document.querySelector(".bread-next");
+const mainNextBtn = document.querySelector(".main-next");
+const breadDiceImg = document.querySelector(".bread-dice-roll-img");
+const mainDiceImg = document.querySelector(".main-dice-roll-img");
 
 const ingredients = {
   bread: ["Uncrustables", "White", "12 Grain", "Potato", "Rye", "Sourdough"],
-  main: [],
+  main: [
+    "Cat Food",
+    "Salami",
+    "Peanut Butter",
+    "Black Forest Ham",
+    "Roast Beef",
+    "Beans",
+  ],
   roughage: [],
   wildMagic: [],
   sauce: [],
@@ -28,17 +42,47 @@ const hideFunction = function (hide, reveal) {
 
 // EVENT LISTENERS
 beginBtn.addEventListener("click", function () {
-  hideFunction(welcomeModal, rollModal);
+  hideFunction(welcomeModal, breadModal);
 });
 
 // Bread choice
+let isBreadRolled = false;
 breadDice.addEventListener("click", function () {
-  // 1. Roll the dice for a random number
-  let breadChoice = rollDice(); // 0 - 5
-  // 2. take number and use it to add bread to sandwich object
-  sandwich.bread = ingredients.bread[breadChoice];
-  console.log(sandwich);
-  // 3. change dice img to correct dice
+  if (isBreadRolled == false) {
+    // 1. Roll the dice for a random number
+    let choice = rollDice(); // 0 - 5
+    // 2. take number and use it to add bread to sandwich object
+    sandwich.bread = ingredients.bread[choice];
+    console.log(sandwich);
+    // 3. change dice img to correct dice
+    breadDiceImg.src = `dice-images/dice${choice + 1}.svg`;
+    // 4. Highlight choice
+    document.querySelector(`.breadchoice${choice}`).style.background =
+      "var(--light-green)";
+    // 4. make next button appear
+    breadNextBtn.classList.remove("hidden");
+  }
+  // Make button nonfunctional after choice
+  isBreadRolled = true;
+});
+breadNextBtn.addEventListener("click", function () {
+  hideFunction(breadModal, mainModal);
+});
 
-  console.log(breadChoice);
+let isMainRolled = false;
+mainDice.addEventListener("click", function () {
+  if (isMainRolled == false) {
+    let choice = rollDice(); // 0 - 5
+    sandwich.main = ingredients.main[choice];
+    console.log(sandwich);
+    // 3. change dice img to correct dice
+    mainDiceImg.src = `dice-images/dice${choice + 1}.svg`;
+    // 4. Highlight choice
+    document.querySelector(`.mainchoice${choice}`).style.background =
+      "var(--light-green)";
+    // 4. make next button appear
+    mainNextBtn.classList.remove("hidden");
+  }
+  // Make button nonfunctional after choice
+  isMainRolled = true;
 });
